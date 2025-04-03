844d6a01fef22aa9258e31b77598d7412642d49f
Ofek Bar shalom <155369472+ofekbarshalom@users.noreply.github.com>
2025-04-03 16:12:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,27 +8,38 @@ using namespace std;
 using namespace graph;
 using namespace algorithms;
 
-// int main() {
-//     // Create a graph with 6 vertices (0 to 5)
-//     Graph g(6);
-//
-//     // Add edges: (u, v, weight)
-//     g.addEdge(0, 1, 4);
-//     g.addEdge(0, 2, 3);
-//     g.addEdge(1, 2, 1);
-//     g.addEdge(1, 3, 2);
-//     g.addEdge(2, 3, 4);
-//     g.addEdge(3, 4, 2);
-//     g.addEdge(4, 5, 6);
-//
-//     cout << "Original Graph:\n";
-//     g.print_graph();
-//
-//     // Run Kruskal's algorithm
-//     Graph mst = Algorithms::kruskal(g);
-//
-//     cout << "\nMinimum Spanning Tree (Kruskal):\n";
-//     mst.print_graph();
-//
-//     return 0;
-// }
+int main() {
+    Graph g(6);
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 2, 3);
+    g.addEdge(1, 2, 1);
+    g.addEdge(1, 3, 2);
+    g.addEdge(2, 3, 4);
+    g.addEdge(3, 4, 2);
+    g.addEdge(4, 5, 6);
+
+    cout << "Original Graph:" << endl;
+    g.print_graph();
+
+    cout << "\nBFS Tree (from vertex 0):" << endl;
+    Graph bfsTree = Algorithms::bfs(g, 0);
+    bfsTree.print_graph();
+
+    cout << "\nDFS Tree (from vertex 0):" << endl;
+    Graph dfsTree = Algorithms::dfs(g, 0);
+    dfsTree.print_graph();
+
+    cout << "\nDijkstra Shortest-Path Tree (from vertex 0):" << endl;
+    Graph dijkstraTree = Algorithms::dijkstra(g, 0);
+    dijkstraTree.print_graph();
+
+    cout << "\nMinimum Spanning Tree (Prim):" << endl;
+    Graph primTree = Algorithms::prim(g);
+    primTree.print_graph();
+
+    cout << "\nMinimum Spanning Tree (Kruskal):" << endl;
+    Graph kruskalTree = Algorithms::kruskal(g);
+    kruskalTree.print_graph();
+
+    return 0;
+}
